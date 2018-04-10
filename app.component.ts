ad108a7944f8f138c43cc381c356febee2d3d31e
Glen Goulter <glenrg@gmail.com>
2018-04-10 13:46:15 -0700

    final version, changes time upon time zone selected

--- app.component.ts
+++ app.component.ts
@@ -7,15 +7,44 @@ import { getLocaleDateTimeFormat } from '@angular/common/src/i18n/locale_data_ap
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onButtonClick(data) { 
+  date = new Date(); //sets the time when page is first displayed.
+  lastTimeZone : number = 1;
+  
+  onButtonClick(data) {
+    this.lastTimeZone = data;
+    this.date = new Date();
     console.log(`Click event is working, data:`, data);
-    console.log(`Date:`, data);
-    //now.setHours(now.getHours() + 1)
-    this.data.setHours(this.data.getHours()+1)
-    console.log(`Date:`, this.data);
+    console.log('thisDate: ', this.date);
+    this.date.setHours(this.date.getHours()+data)
+    console.log(`thisDate: again`, this.date);  
   }
-  data = new Date();
 }
+
+// export class AppComponent {
+//   date = new Date();
+//   onButtonClick(data) {
+//     let date = new Date();
+//     console.log(`Click event is working, data:`, data);
+//     console.log('thisDate: ', this.date);
+//     console.log('****************Date: ', date);
+//     date.setHours(this.date.getHours()+data)
+//     this.date = date;
+//     console.log(`Date:`, this.date);
+
+//   }
+// }
+
+// export class AppComponent {
+//   onButtonClick(data) { 
+//     console.log(`Click event is working, data:`, data);
+//     console.log(`Date:`, data);
+//     //now.setHours(now.getHours() + 1)
+//     this.data.setHours(this.data.getHours()+1)
+//     console.log(`Date:`, this.data);
+//   }
+//   data = new Date();
+// }
+
 
 // export class AppComponent {
 //   _date = new Date();
